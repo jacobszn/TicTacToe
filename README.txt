3489313c9455f621fde5f7dc3e22f097e06b2c83
Jacob <official.jacobsibi@gmail.com>
2021-04-08 12:21:40 +1200

    Single game of TicTacToe

---
+++ README.txt
@@ -0,0 +1,8 @@
+| Game of TicTacToe |
+
+Next Steps:
+
+- Implement restart function
+- Implement draw screen output
+- Implement a more aesthetic user interface
+- Implement score keeper
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,6 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		TicTacToe tictactoe = new TicTacToe();
-
+		new TicTacToe();
 	}
-
 }
